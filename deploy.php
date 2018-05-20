6546ce7d686fd6e87915bc0421653352669e23f7
Asper <asperwon@gmail.com>
2018-05-20 14:45:04 +0800

    a

--- deploy.php
+++ deploy.php
@@ -6,24 +6,11 @@ require 'recipe/laravel.php';
 // Project
 set('project_name', getenv('CI_PROJECT_NAME'));
 set('repository', getenv('CI_REPOSITORY_URL'));
-set('stage', getenv('CI_ENVIRONMENT_NAME'));
 
 // host
 set('deploy_path', getenv('DEPLOY_PATH'));
 set('deploy_host', getenv('DEPLOY_SERVER'));
 
-print_r([
-    getenv('CI_PROJECT_NAME'),
-    getenv('CI_REPOSITORY_URL'),
-    getenv('CI_ENVIRONMENT_NAME'),
-    getenv('DEPLOY_PATH'),
-    getenv('DEPLOY_SERVER'),
-    get('deploy_host'),
-    get('stage'),
-]);
-
-// [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true);
 
 // Shared files/dirs between deploys
 add('shared_files', []);
@@ -34,9 +21,7 @@ add('writable_dirs', []);
 
 
 // Hosts
-
 host(get('deploy_host'))
-    // ->stage(get('stage'))
     ->user('deployer')
     ->set('deploy_path', '{{deploy_path}}/{{project_name}}');
 
