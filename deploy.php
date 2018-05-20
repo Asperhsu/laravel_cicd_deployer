c43a42a1010002d6f0ddba1338020c101d0e428f
Asper <asperwon@gmail.com>
2018-05-20 15:40:29 +0800

    a

--- deploy.php
+++ deploy.php
@@ -18,22 +18,19 @@ add('shared_dirs', []);
 // Writable dirs by web server
 add('writable_dirs', []);
 
-// set('allow_anonymous_stats', false);
-// set('writable_mode', 'chmod');
-
 
 // Hosts
 host(get('deploy_host'))
     ->user('deployer')
     ->addSshOption('StrictHostKeyChecking', 'no')
-    ->set('deploy_path', '/var/www/{{project_name}}');
+    ->set('deploy_path', '{{deploy_path}}/{{project_name}}');
 
 // Tasks
-task('artisan:optimize', function () {
-});   // @overwrite
+// task('artisan:optimize', function () {
+// });   // @overwrite
 
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
 
 // Migrate database before symlink new release.
-// before('deploy:symlink', 'artisan:migrate');
+before('deploy:symlink', 'artisan:migrate');
