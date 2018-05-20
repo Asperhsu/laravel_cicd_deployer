f51993ea7c2e95c23084a1148d76b5e2a02216a1
Asper <asperwon@gmail.com>
2018-05-20 14:30:32 +0800

    a

--- deploy.php
+++ deploy.php
@@ -3,11 +3,15 @@ namespace Deployer;
 
 require 'recipe/laravel.php';
 
-// Project name
-set('application', getenv('CI_PROJECT_NAME'));
-
-// Project repository
+// Project
+set('project_name', getenv('CI_PROJECT_NAME'));
 set('repository', getenv('CI_REPOSITORY_URL'));
+set('stage', getenv('CI_ENVIRONMENT_NAME'));
+
+// host
+set('deploy_path', getenv('DEPLOY_PATH'));
+set('deploy_host', getenv('DEPLOY_SERVER'));
+
 
 // [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', true);
@@ -22,9 +26,10 @@ add('writable_dirs', []);
 
 // Hosts
 
-host('project.com')
+host(get('deploy_host'))
+    ->stage(get('stage'))
     ->user('deployer')
-    ->set('deploy_path', '~/{{application}}');
+    ->set('deploy_path', '{{deploy_path}}/{{project_name}}');
 
 // Tasks
 
@@ -36,5 +41,4 @@ task('build', function () {
 after('deploy:failed', 'deploy:unlock');
 
 // Migrate database before symlink new release.
-
 before('deploy:symlink', 'artisan:migrate');
